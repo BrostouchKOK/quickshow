b14804c693616050bc631415c4a769d526827bdf
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-11-27 17:05:03 +0700

    fix email_address to email_addresses

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,7 +9,7 @@ const syncUserCreation = inngest.createFunction(
     {id: "sync-user-from-clerk"},
     {event: 'clerk/user.created'},
     async ({event})=>{
-        const {id, first_name, last_name, email_address, image_url} = event.data;
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
         const userData= {
             _id: id,
             email: email_addresses[0].email_address,
@@ -35,10 +35,10 @@ const syncUserUpdation = inngest.createFunction(
     {id: "update-user-from-clerk"},
     {event: 'clerk/user.updated'},
     async ({event})=>{
-        const {id, first_name, last_name, email_address, image_url} = event.data;
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
         const userData= {
             _id: id,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url,
         }
